a2a410958698f14da353e30957c978b71a781969
aditya-sengupta <aditya.sengupta@berkeley.edu>
2021-06-23 10:53:59 -0700

    cleanup

--- ancillary_code.py
+++ ancillary_code.py
@@ -8,18 +8,18 @@ import pysao
 import numpy as np
 import sys
 import time
-import functions
+import ao_utils
 
 #initialize; no need to load this more than once
 #for full frame:
 
-#a=shmlib.shm('/tmp/ca01dit.im.shm') 
-#im=shmlib.shm('/tmp/ca01im.im.shm')
+#a = shmlib.shm('/tmp/ca01dit.im.shm') 
+#im = shmlib.shm('/tmp/ca01im.im.shm')
 
 #for 320x320 subarray
 
-a=shmlib.shm('/tmp/ca03dit.im.shm') 
-im=shmlib.shm('/tmp/ca03im.im.shm')
+a = shmlib.shm('/tmp/ca03dit.im.shm') 
+im = shmlib.shm('/tmp/ca03im.im.shm')
 
 def expt(t):
 	'''
@@ -27,8 +27,8 @@ def expt(t):
 
 	for the large array the smallest exposure time is around 1e-5
 	'''
-	dit=a.get_data()
-	dit[0][0]=t; a.set_data(dit)
+	dit = a.get_data()
+	dit[0][0] = t; a.set_data(dit)
 
 def getim():
 	return im.get_data()
@@ -36,104 +36,104 @@ def vim(): #view image in ds9
 	ds9.view(getim())
 
 def vmtf(): #view image MTF
-	imm=getim()
-	mtf=np.abs(np.fft.fftshift(np.fft.fft2(imm)))
+	imm = getim()
+	mtf = np.abs(np.fft.fftshift(np.fft.fft2(imm)))
 	ds9.view(mtf)
 
 def stack(n):
-	ims=np.zeros(getim().shape)
+	ims = np.zeros(getim().shape)
 	for i in range(n):
-		ims=ims+getim()
-	ims=ims/n
+		ims = ims+getim()
+	ims = ims/n
 	return ims
 
-mtf = lambda im: np.abs(np.fft.fftshift(np.fft.fft2(im)))
+mtf  =  lambda im: np.abs(np.fft.fftshift(np.fft.fft2(im)))
 
 
 #DM commands
 
-b=shmlib.shm('/tmp/dm02itfStatus.im.shm')
-status=b.get_data()
-status[0,0]=1
+b = shmlib.shm('/tmp/dm02itfStatus.im.shm')
+status = b.get_data()
+status[0,0] = 1
 b.set_data(status)
-dmChannel=shmlib.shm('/tmp/dm02disp01.im.shm')
+dmChannel = shmlib.shm('/tmp/dm02disp01.im.shm')
 
 def getdmc(): # read current command applied to the DM
 	return dmChannel.get_data()
 def applydmc(cmd): #apply command to the DM
-	indneg=np.where(cmd<0)
+	indneg = np.where(cmd<0)
 	if len(indneg[0])>0:
-		cmd[indneg]=0 #minimum value is zero
+		cmd[indneg] = 0 #minimum value is zero
 		print('saturating DM zeros!')
-	indneg=None
-	indpos=np.where(cmd>1)
+	indneg = None
+	indpos = np.where(cmd>1)
 	if len(indpos[0])>0:
-		cmd[indpos]=1 #maximum value is 1
+		cmd[indpos] = 1 #maximum value is 1
 		print('saturating DM ones!')
-	indpos=None
+	indpos = None
 	dmChannel.set_data(cmd)
 
-dmcini=getdmc()
-dmzero=np.zeros(dmcini.shape,dtype=np.float32)
-applyzero = lambda : applydmc(dmzero)
+dmcini = getdmc()
+dmzero = np.zeros(dmcini.shape,dtype = np.float32)
+applyzero  =  lambda : applydmc(dmzero)
 
 
 #WFS slopes
-port="5556"
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
+port = "5556"
+context  =  zmq.Context()
+socket  =  context.socket(zmq.REQ)
 socket.connect("tcp://128.114.22.20:%s" % port)
 
 def getPupilSize(sock):
     socket.send_string("pupSize");
-    data=socket.recv()
-    pupSize = np.frombuffer(data, dtype=np.int32)
+    data = socket.recv()
+    pupSize  =  np.frombuffer(data, dtype = np.int32)
     return pupSize
 
-pupSize = getPupilSize(socket)[0]
+pupSize  =  getPupilSize(socket)[0]
 
 def getwf():
     socket.send_string("wavefront");
-    data=socket.recv()
-    wf = np.frombuffer(data, dtype=np.float32).reshape(pupSize, pupSize)
+    data = socket.recv()
+    wf  =  np.frombuffer(data, dtype = np.float32).reshape(pupSize, pupSize)
     return wf
 
 def stackwf(n): #average some number of frames of wavefront
-	imw=np.zeros(getwf().shape)
+	imw = np.zeros(getwf().shape)
 	for i in range(n):
-		imw=imw+getwf()
-	imw=imw/n
+		imw = imw+getwf()
+	imw = imw/n
 	return imw
 
 def getSlopes(): #something still wrong with slopes...
     socket.send_string("slopes");
-    data=socket.recv()
-    slopes = np.frombuffer(data, dtype=np.float32).reshape(pupSize, 2*pupSize)
-    sx = slopes[:,:pupSize]
-    sy = slopes[:,pupSize:]
+    data = socket.recv()
+    slopes  =  np.frombuffer(data, dtype = np.float32).reshape(pupSize, 2*pupSize)
+    sx  =  slopes[:,:pupSize]
+    sy  =  slopes[:,pupSize:]
     return np.array([sx, sy])
 
 def stackSlopes(n): #average some number of frames of slopes
-	ims=np.zeros(getSlopes().shape)
+	ims = np.zeros(getSlopes().shape)
 	for i in range(n):
-		ims=ims+getSlopes()
-	ims=ims/n
+		ims = ims+getSlopes()
+	ims = ims/n
 	return ims
 
 
-#wf = getWavefront()
-#sx,sy = getSlopes()
+#wf  =  getWavefront()
+#sx,sy  =  getSlopes()
 
 '''
 # Push each actuator
 for k in range(0,32):
     for l in range(0,32):
-     cmd=cmd*0;
-     cmd[k][l] = 1
+     cmd = cmd*0;
+     cmd[k][l]  =  1
      dmChannel.set_data(cmd)
      time.sleep(0.2)
 '''
 
 
-##fig,axs=plt.subplots(ncols=1,nrows=2)
+##fig,axs = plt.subplots(ncols = 1,nrows = 2)
 
--- zern_opt.py
+++ zern_opt.py
@@ -4,136 +4,137 @@ code to attempt Zernike optimization, aka "focal plane sharpening"
 
 from ancillary_code import *
 import numpy as np
+from numpy import float32
 import time
-import functions
+import ao_utils
 
 #setup
-#bestflat=np.load('bestflat.npy')
-#bestflat=np.load('bestflat_shwfs.npy')
-bestflat=np.load('bestflat_zopt.npy') #for bootstrapping
+#bestflat = np.load('bestflat.npy')
+#bestflat = np.load('bestflat_shwfs.npy')
+bestflat = np.load('bestflat_zopt.npy') #for bootstrapping
 applydmc(bestflat)
-dmcini=getdmc()
+dmcini = getdmc()
 
 #applytip/tilt, manually steering the PSF around until the PSF is sufficiently off the FPM and not saturating DM commands
-ydim,xdim=dmcini.shape
-grid=np.mgrid[0:ydim,0:xdim].astype(float32)
-ygrid,xgrid=grid[0]-ydim/2,grid[1]-xdim/2
-tip,tilt=(ygrid+ydim/2)/ydim,(xgrid+xdim/2)/xdim #min value is zero, max is one
+ydim,xdim = dmcini.shape
+grid = np.mgrid[0:ydim,0:xdim].astype(float32)
+ygrid,xgrid = grid[0]-ydim/2,grid[1]-xdim/2
+tip,tilt = (ygrid+ydim/2)/ydim,(xgrid+xdim/2)/xdim #min value is zero, max is one
 
 #DM aperture:
-undersize=27/32 #assuming 27 of the 32 actuators are illuminated
-rho,phi=functions.polar_grid(xdim,xdim*undersize)
-cenaperture=np.zeros(rho.shape).astype(float32)
-indapcen=np.where(rho>0)
-cenaperture[indapcen]=1
+undersize = 27/32 #assuming 27 of the 32 actuators are illuminated
+rho,phi = ao_utils.polar_grid(xdim,xdim*undersize)
+cenaperture = np.zeros(rho.shape).astype(float32)
+indapcen = np.where(rho>0)
+cenaperture[indapcen] = 1
 
-aperture=np.load('DMmap.npy').astype(float32) #actual aperture, from close_SHWFS_loop.py
+aperture = np.load('DMmap.npy').astype(float32) #actual aperture, from close_SHWFS_loop.py
 
 #from comparing cenaperture and aperture, the actual aperture is shifted down and to the right (in ds9) each by 1 pixel from the center
-yapcen,xapcen=ydim/2.-0.5-1,xdim/2.-0.5-1
-rap=np.sqrt((grid[0]-yapcen)**2.+(grid[1]-xapcen)**2.)
-rap[np.where(rap>xdim/2.*undersize)]=0.
-rhoap=rap/np.max(rap)
-phiap=np.arctan2(grid[1]-yapcen,grid[0]-xapcen)
-indap=np.where(rhoap>0)
+yapcen,xapcen = ydim/2.-0.5-1,xdim/2.-0.5-1
+rap = np.sqrt((grid[0]-yapcen)**2.+(grid[1]-xapcen)**2.)
+rap[np.where(rap>xdim/2.*undersize)] = 0.
+rhoap = rap/np.max(rap)
+phiap = np.arctan2(grid[1]-yapcen,grid[0]-xapcen)
+indap = np.where(rhoap>0)
 
-remove_piston = lambda dmc: dmc-np.mean(dmc[indap]) #function to remove piston from dm command to have zero mean (must be intermediate)
+remove_piston  =  lambda dmc: dmc-np.mean(dmc[indap]) #function to remove piston from dm command to have zero mean (must be intermediate)
 
-IMtt=np.array([(tip*aperture).flatten(),(tilt*aperture).flatten()])
-CMtt=np.linalg.pinv(IMtt,rcond=1e-5)
+IMtt = np.array([(tip*aperture).flatten(),(tilt*aperture).flatten()])
+CMtt = np.linalg.pinv(IMtt,rcond = 1e-5)
 def rmtt(ph): #remove tip/tilt from aperture
-	coeffs=np.dot(np.vstack((ph*aperture).flatten()).T,CMtt) 
-	lsqtt=np.dot(IMtt.T,coeffs.T).reshape(tilt.shape).astype(float32)
+	coeffs = np.dot(np.vstack((ph*aperture).flatten()).T,CMtt) 
+	lsqtt = np.dot(IMtt.T,coeffs.T).reshape(tilt.shape).astype(float32)
 	return ph*aperture-lsqtt
 
 def applytip(amp): #apply tip; amp is the P2V in DM units
-	dmc=getdmc()
-	dmctip=amp*tip
-	dmc=remove_piston(dmc)+remove_piston(dmctip)+0.5
+	dmc = getdmc()
+	dmctip = amp*tip
+	dmc = remove_piston(dmc)+remove_piston(dmctip)+0.5
 	applydmc(dmc*aperture)
 
 def applytilt(amp): #apply tilt; amp is the P2V in DM units
-	dmc=getdmc()
-	dmctilt=amp*tilt
-	dmc=remove_piston(dmc)+remove_piston(dmctilt)+0.5
+	dmc = getdmc()
+	dmctilt = amp*tilt
+	dmc = remove_piston(dmc)+remove_piston(dmctilt)+0.5
 	applydmc(dmc*aperture)
 
-ttdmc=getdmc()
+ttdmc = getdmc()
 #np.save('bestflat_offfpm.npy',ttdmc)
 
 #Zernike polynomials
-nmarr=[]
-norder=15 #how many radial Zernike orders to look at
+nmarr = []
+norder = 15 #how many radial Zernike orders to look at
 for n in range(2,norder):
 	for m in range(-n,n+1,2):
 		nmarr.append([n,m])
 
-def funz(n,m,amp,bestflat=ttdmc): #apply zernike to the DM
-	z=functions.zernike(n,m,rhoap,phiap)/2
-	zdm=amp*(z.astype(float32))
-	dmc=remove_piston(bestflat)+remove_piston(rmtt(zdm))+0.5
+def funz(n,m,amp,bestflat = ttdmc): #apply zernike to the DM
+	z = ao_utils.zernike(n,m,rhoap,phiap)/2
+	zdm = amp*(z.astype(float32))
+	dmc = remove_piston(bestflat)+remove_piston(rmtt(zdm))+0.5
 	applydmc(dmc*aperture)
 	return dmc
 
-namp=100 #how many grid points to walk through Zernike amplitude coefficients
-amparr=np.linspace(-1,1,namp)
+namp = 100 #how many grid points to walk through Zernike amplitude coefficients
+amparr = np.linspace(-1,1,namp)
 
-tsleep=0.05 #time to sleep in between applying DM commands and grabbing images, determined in optt of genIM.py
+tsleep = 0.05 #time to sleep in between applying DM commands and grabbing images, determined in optt of genIM.py
 
 #focus loop
-n,m=2,0
+n,m = 2,0
 applyzero()
-optarr=np.zeros(namp)
+optarr = np.zeros(namp)
 for a in range(namp):
-	dmz=funz(n,m,amparr[a])
+	dmz = funz(n,m,amparr[a])
 	time.sleep(tsleep)
-	ims=stack(10)
-	optarr[a]=np.max(ims)/np.sum(ims)
-	print('focus amp='+str(amparr[a]))
-optamp=amparr[np.where(optarr==np.max(optarr))]
-dmzo=funz(n,m,optamp[0])
-bestflat=dmzo
-plt.subplots(figsize=(17,5))
-plt.plot(amparr,optarr,label='n,m='+str(n)+','+str(m))
+	ims = stack(10)
+	optarr[a] = np.max(ims)/np.sum(ims)
+	print('focus amp = '+str(amparr[a]))
+optamp = amparr[np.where(optarr == np.max(optarr))]
+dmzo = funz(n,m,optamp[0])
+bestflat = dmzo
+plt.subplots(figsize = (17,5))
+plt.plot(amparr,optarr,label = 'n,m = '+str(n)+','+str(m))
 plt.ylabel('normalized SR (unitless)')
 plt.xlabel('Zernike amplitude (DM units)')
 for i in range(len(nmarr)):
-	n,m=nmarr[i]
-	if n==2 and m==0:
+	n,m = nmarr[i]
+	if n == 2 and m == 0:
 		continue #skip focus, which is already optimized
-	print ('optimizing (n,m)=('+str(n)+','+str(m)+')')
-	optarr=np.zeros(namp)
+	print ('optimizing (n,m) = ('+str(n)+','+str(m)+')')
+	optarr = np.zeros(namp)
 	for a in range(namp):
-		dmz=funz(n,m,amparr[a],bestflat=bestflat)
+		dmz = funz(n,m,amparr[a],bestflat = bestflat)
 		time.sleep(tsleep)
-		ims=stack(10)
-		optarr[a]=np.max(ims)/np.sum(ims)
-		print('amp='+str(amparr[a]))
-	optamp=amparr[np.where(optarr==np.max(optarr))]
-	dmzopt=funz(n,m,optamp[0],bestflat=bestflat)
-	bestflat=dmzopt
+		ims = stack(10)
+		optarr[a] = np.max(ims)/np.sum(ims)
+		print('amp = '+str(amparr[a]))
+	optamp = amparr[np.where(optarr == np.max(optarr))]
+	dmzopt = funz(n,m,optamp[0],bestflat = bestflat)
+	bestflat = dmzopt
 	applydmc(bestflat)
 	time.sleep(tsleep)
-	plt.plot(amparr,optarr,label='n,m='+str(n)+','+str(m))
-plt.legend(bbox_to_anchor=(1.05,1),loc='upper left',ncol=5)
+	plt.plot(amparr,optarr,label = 'n,m = '+str(n)+','+str(m))
+plt.legend(bbox_to_anchor = (1.05,1),loc = 'upper left',ncol = 5)
 plt.tight_layout()
 
 #np.save('bestflat_zopt_daren.npy',bestflat) #version for when Darren mooves the PSD off the FPM
 np.save('bestflat_zopt.npy',bestflat)
 
 #testing sine wave code:
-ycen,xcen=ydim/2-0.5,xdim/2-0.5
-rgrid=lambda pa:(grid[0]-ycen)/grid[0][-1,0]*np.cos(pa*np.pi/180)+(grid[1]-xcen)/grid[0][-1,0]*np.sin(pa*np.pi/180)
+ycen,xcen = ydim/2-0.5,xdim/2-0.5
+rgrid = lambda pa:(grid[0]-ycen)/grid[0][-1,0]*np.cos(pa*np.pi/180)+(grid[1]-xcen)/grid[0][-1,0]*np.sin(pa*np.pi/180)
 def dmsin(amp,freq,pa): 
-	sin=amp*0.5*np.sin(2*np.pi*freq*rgrid(pa))
-	sindm=sin.astype(float32)
-	dmc=remove_piston(bestflat)+sindm+0.5
+	sin = amp*0.5*np.sin(2*np.pi*freq*rgrid(pa))
+	sindm = sin.astype(float32)
+	dmc = remove_piston(bestflat)+sindm+0.5
 	applydmc(dmc*aperture)
 
 def dmsin2(amp1,amp2,freq1,freq2,pa1,pa2): 
-	sin1=amp1*0.5*np.sin(2*np.pi*freq1*rgrid(pa1))
-	sin2=amp2*0.5*np.sin(2*np.pi*freq2*rgrid(pa2))
-	sindm1=sin1.astype(float32)
-	sindm2=sin2.astype(float32)
-	dmc=remove_piston(bestflat)+sindm1+sindm2+0.5
+	sin1 = amp1*0.5*np.sin(2*np.pi*freq1*rgrid(pa1))
+	sin2 = amp2*0.5*np.sin(2*np.pi*freq2*rgrid(pa2))
+	sindm1 = sin1.astype(float32)
+	sindm2 = sin2.astype(float32)
+	dmc = remove_piston(bestflat)+sindm1+sindm2+0.5
 	applydmc(dmc*aperture)
