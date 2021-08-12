36116c29571bece2409bd32267b115f51abc59d2
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2021-08-11 19:33:13 -0700

    the psd axis is no longer standardized and i need to adjust to this new reality

--- src/optics/compute_cmd_int.py
+++ src/optics/compute_cmd_int.py
@@ -15,12 +15,13 @@ from scipy.optimize import newton
 from .image import getdmc, applydmc, stack, getim
 from .tt import rhoap, phiap, processim
 from .ao import polar_grid, zernike
+from ..utils import joindata
 
 #initial setup: apply best flat, generate DM grid to apply future shapes
 dmcini = getdmc()
 ydim, xdim = dmcini.shape
 grid = np.mgrid[0:ydim, 0:xdim].astype(np.float32)
-bestflat = np.load('/home/lab/asengupta/data/bestflats/bestflat.npy') #load bestflat, which should be an aligned FPM
+bestflat = np.load(joindata("bestflats/bestflat.npy")) #load bestflat, which should be an aligned FPM
 applydmc(bestflat)
 imflat = stack(100)
 
