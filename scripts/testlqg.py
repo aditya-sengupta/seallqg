3de2bf22dd606875d3371078277e86f0e4c1ccdc
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2021-10-20 15:32:02 +0100

    3x improvement again

--- scripts/testlqg.py
+++ scripts/testlqg.py
@@ -11,14 +11,14 @@ s, m, p = 2, 1, 1
 
 M = np.random.randn(s,s)
 eigvals = np.random.uniform(-1, 1, s)
-# eigvals = eigvals / (1.01 * np.max(np.abs(eigvals)))
+eigvals = eigvals / (1.01 * np.max(np.abs(eigvals)))
 A = np.linalg.inv(M) @ np.diag(eigvals) @ M
 B = np.random.randn(s, p)
 C = np.random.randn(m, s)
-W, V = np.eye(s), np.eye(m)
-# W, V = Wr @ Wr.T, Vr @ Vr.T
-Q = 1e4 * np.eye(s)
-R = 1e-2 * np.eye(p)
+Wr, Vr = np.random.randn(s, s), np.random.randn(m, m)
+W, V = Wr @ Wr.T, Vr @ Vr.T
+Q = 1 * np.eye(s)
+R = 1e-6 * np.eye(p)
 
 klqg = KalmanLQG(A, B, C, W, V, Q, R)
 # Ppr = solve_dare(A.T, C.T * 0, W, V)
