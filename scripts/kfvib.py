934dc608844974e1471c5f6c05cc1ccfd857e2d1
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2021-11-13 11:38:54 +0000

    oversleeping fix i think

--- scripts/kfvib.py
+++ scripts/kfvib.py
@@ -11,6 +11,7 @@ from src.utils import joindata
 from src.controllers import make_kalman_controllers
 from src.experiments.schedules import *
 from src.experiments.exp_utils import record_experiment, control_schedule_from_law
+from src.constants import fs, dt
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -29,9 +30,9 @@ if f == 5:
 elif f == 1:
     ol = np.load(joindata("openloop", "ol_f_1_z_stamp_03_11_2021_13_58_53.npy")) * dmc2wf
 
-ol_spectra = [genpsd(ol[:,i], dt=0.01) for i in range(2)]
+ol_spectra = [genpsd(ol[:,i], dt=dt) for i in range(2)]
 
-ident = SystemIdentifier(ol, fs=100)
+ident = SystemIdentifier(ol, fs=fs)
 klqg = ident.make_klqg_from_openloop()
 def recompute_schedules(klqg):
     kalman_integrate, kalman_lqg = make_kalman_controllers(klqg)
@@ -97,14 +98,14 @@ def get_ol_cl_rms(zvals):
         data.append([olc, cl, rms_ratio])
     return data
 
-def plot_cl_rtf(data, dt=datetime.now().strftime("%d_%m_%Y_%H_%M_%S"), save=True):
+def plot_cl_rtf(data, timestamp=datetime.now().strftime("%d_%m_%Y_%H_%M_%S"), save=True):
     fig, axs = plt.subplots(2, figsize=(9,9))
     fig.tight_layout(pad=4.0)
     plt.suptitle("LQG rejection")
     for mode in range(2):
         olc, cl, rms_ratio = data[mode]
-        f_ol, p_ol = genpsd(olc, dt=0.01)
-        f_cl, p_cl = genpsd(cl, dt=0.01)
+        f_ol, p_ol = genpsd(olc, dt=dt)
+        f_cl, p_cl = genpsd(cl, dt=dt)
         axs[mode].loglog(f_ol, p_ol, label="Open-loop")
         axs[mode].loglog(f_cl, p_cl, label="Closed-loop")
         axs[mode].loglog(f_cl, p_cl / p_ol, label="Rejection TF")
@@ -112,7 +113,7 @@ def plot_cl_rtf(data, dt=datetime.now().strftime("%d_%m_%Y_%H_%M_%S"), save=True
         axs[mode].set_xlabel("Frequency (Hz)")
         axs[mode].set_ylabel(r"Power (DM $units^2/Hz$)")
         axs[mode].set_title(f"Mode {mode}, CL/OL RMS {rms_ratio}")
-        fname = f"../plots/cl_lqg_{dt}.pdf"
+        fname = f"../plots/cl_lqg_{timestamp}.pdf"
         if save:
             plt.savefig(joindata(fname))
     plt.show()
@@ -122,8 +123,8 @@ def plot_cl_rtf(data, dt=datetime.now().strftime("%d_%m_%Y_%H_%M_%S"), save=True
 
 if __name__ == "__main__":
     #times, zvals = record_olnone(t=10)
-    times, zvals, dt = lqg(klqg, t=10)
+    times, zvals, timestamp = lqg(klqg, t=10)
     data = get_ol_cl_rms(zvals * dmc2wf)
     print(f"RMS ratios: {[float(x[2]) for x in data]}")
     if input("Plot? (y/n) ") == 'y':
-        plot_cl_rtf(data, dt)
+        plot_cl_rtf(data, timestamp)
