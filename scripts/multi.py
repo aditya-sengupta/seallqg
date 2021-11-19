e41fe5f598fec3bfb3f52c90cfec4870d4254e01
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2021-11-19 06:45:30 -0800

    a functioning thing!

--- scripts/multi.py
+++ scripts/multi.py
@@ -1,32 +1,26 @@
-from multiprocessing import Process, Lock
+from multiprocessing import Process
 from time import monotonic_ns as mns
-from math import ceil
-from functools import partial
 
 from sealrtc import spinlock_till, spinlock, spin
 
 dt = 0.01
 dur = 0.2
 
-def act(lock, message, delay, t0):
+def act(message, delay, t0):
     spinlock_till(t0)
     def iteration():
-        lock.acquire()
-        try:
-            print(f"{message} {(mns() - t0) / 1e9}")
-            spinlock(delay)
-        finally:
-            lock.release()
+        print(f"{message} {(mns() - t0) / 1e9}")
+        spinlock(delay)
+
     
     spin(iteration, dt, dur)
 
 if __name__ == '__main__':
-    lock = Lock()
-
     processes = []
     t0 = mns()
-    processes.append(Process(target=act, args=(lock, "Dist", 0.001, t0)))
-    processes.append(Process(target=act, args=(lock, "Loop", 0.005, t0+1e6)))
+
+    processes.append(Process(target=act, args=("Dist", 0.006, t0)))
+    processes.append(Process(target=act, args=("Loop", 0.005, t0+1e6)))
 
     for p in processes:
         p.start()
